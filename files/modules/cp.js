9af6a0e3cd941b046faa95abd798eec7bcb3e2af
Artiom Filkevich <artiom.filkevich@gmail.com>
2022-06-11 23:52:50 +0300

    refactor: cp operation rewrite to streams

--- files/modules/cp.js
+++ files/modules/cp.js
@@ -1,19 +1,37 @@
 import { createReadStream, createWriteStream } from 'fs'
+import { parse, resolve } from 'path'
 import { pathValidator } from '../../validators/index.js'
+import { printInvalidInputMsg } from '../../msg/index.js'
 
-const cp = args => {
-  if (args.length < 2) throw new Error('Wrong arguments quantity')
+const cp = async args => {
+  const isArgsValid = args.length === 2
+  if (!isArgsValid) printInvalidInputMsg()
+  else {
+    try {
+      const [ from, to ] = args
+      const { normalizedPath: fromPath, isPathExists: isFromPathValid } = pathValidator(from)
+      const { normalizedPath: toPath, isPathExists: isToPathValid } = pathValidator(to, true)
 
-  const [ from, to ] = args
-  const { normalizedPath: fromPath, isPathExists: isFromPathValid } = pathValidator(from)
-  const { normalizedPath: toPath, isPathExists: isToPathValid } = pathValidator(to, true)
+      const { name, ext } = parse(fromPath)
+      const toFilePath = resolve(toPath, name + ext)
+      const { isPathExists: isCopyExists } = pathValidator(toFilePath)
 
-  if (isFromPathValid && isToPathValid) {
-    const readable = createReadStream(fromPath, { encoding: 'utf8', highWaterMark: 16 * 1024 })
-    const writable = createWriteStream(toPath)
-    readable.pipe(writable)
+      if (isFromPathValid && isToPathValid && !isCopyExists) {
+        await new Promise((resolve, reject) => {
+          const readable = createReadStream(fromPath, { encoding: 'utf8', highWaterMark: 16 * 1024 })
+          const writable = createWriteStream(toFilePath)
+          readable.on('data', chunk => writable.write(chunk))
+          readable.on('error', err => reject(err))
+          readable.on('end', () => resolve())
+          writable.on('error', err => reject(err))
+        })
+      }
+      else throw new Error('There is no such directory/file or copy of file already exists')
+    }
+    catch(err) {
+      throw new Error(err)
+    }
   }
-  else throw new Error('There is no such directory or file')
 }
 
 export default cp
