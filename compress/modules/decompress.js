380715cd4dc4ac39177b4b9a312e98d8cf09ad48
Artiom Filkevich <artiom.filkevich@gmail.com>
2022-06-12 00:16:05 +0300

    refactor: compress and decompress operations

--- compress/modules/decompress.js
+++ compress/modules/decompress.js
@@ -1,31 +1,38 @@
 import { createBrotliDecompress } from 'zlib'
 import { createReadStream, createWriteStream } from 'fs'
-import { pathValidator } from '../../validators/index.js'
 import { pipeline } from 'stream'
 import { resolve, parse } from 'path'
+import { pathValidator } from '../../validators/index.js'
+import { printInvalidInputMsg } from '../../msg/index.js'
 
-const decompress = args => {
-  if (args.length < 2) throw new Error('Wrong arguments quantity')
-
-  const [ source, destination ] = args
-  const { normalizedPath: sourcePath, isPathExists: isSourcePathValid } = pathValidator(source)
-  const { normalizedPath: destinationPath, isPathExists: isDestinationPathValid } = pathValidator(destination)
-
-  const { name } = parse(sourcePath)
-  const resolvedDestinationPath = resolve(destinationPath, name)
-  const { isPathExists: isDecompressedFileExists } = pathValidator(resolvedDestinationPath)
-
-  if (isSourcePathValid, isDestinationPathValid, !isDecompressedFileExists) {
-    console.log('in block')
-    const sourceStream = createReadStream(sourcePath)
-    const destinationStream = createWriteStream(resolvedDestinationPath)
-    const brotly = createBrotliDecompress()
-
-    pipeline(sourceStream, brotly, destinationStream, err => { 
-      if (err) throw new Error(err) 
-    })
+const decompress = async args => {
+  const isArgsValid = args.length === 2
+  if (!isArgsValid) printInvalidInputMsg()
+  else {
+    try {
+      const [ source, destination ] = args
+      const { normalizedPath: sourcePath, isPathExists: isSourcePathValid } = pathValidator(source)
+      const { normalizedPath: destinationPath, isPathExists: isDestinationPathValid } = pathValidator(destination)
+    
+      const { name } = parse(sourcePath)
+      const resolvedDestinationPath = resolve(destinationPath, name)
+      const { isPathExists: isDecompressedFileExists } = pathValidator(resolvedDestinationPath)
+    
+      if (isSourcePathValid, isDestinationPathValid, !isDecompressedFileExists) {
+        const sourceStream = createReadStream(sourcePath)
+        const destinationStream = createWriteStream(resolvedDestinationPath)
+        const brotly = createBrotliDecompress()
+    
+        pipeline(sourceStream, brotly, destinationStream, err => { 
+          if (err) throw new Error(err) 
+        })
+      }
+      else throw new Error('There is no such directory or file already exisists')
+    }
+    catch(err) {
+      throw new Error(err)
+    }
   }
-  else throw new Error('There is no such directory or file already exisists')
 }
 
 export default decompress
