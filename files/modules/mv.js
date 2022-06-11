8cad801427be7eb682ec20d9848f2ed7578d3baf
Artiom Filkevich <artiom.filkevich@gmail.com>
2022-06-11 23:59:23 +0300

    refactor: move operation

--- files/modules/mv.js
+++ files/modules/mv.js
@@ -1,19 +1,27 @@
-import { renameSync } from 'fs'
+import { rename } from 'fs/promises'
 import { pathValidator } from '../../validators/index.js'
 import { resolve, parse } from 'path'
+import { printInvalidInputMsg } from '../../msg/index.js'
 
-const mv = args => {
-  if (args.length < 2) throw new Error('Wrong arguments quantity')
-
-  const [ from, to ] = args
-  const { normalizedPath: fromPath, isPathExists: isFromPathValid } = pathValidator(from)
-  const { normalizedPath: toPath, isPathExists: isToPathValid } = pathValidator(to, true)
-
-  const { name, ext } = parse(fromPath)
-  const toPathWithFileName = resolve(toPath, name + ext)
-
-  if (isFromPathValid && isToPathValid) renameSync(fromPath, toPathWithFileName)
-  else throw new Error('There is no such directory or file')
+const mv = async args => {
+  const isArgsValid = args.length === 2
+  if (!isArgsValid) printInvalidInputMsg()
+  else {
+    try {
+      const [ from, to ] = args
+      const { normalizedPath: fromPath, isPathExists: isFromPathValid } = pathValidator(from)
+      const { normalizedPath: toPath, isPathExists: isToPathValid } = pathValidator(to, true)
+    
+      const { name, ext } = parse(fromPath)
+      const toPathWithFileName = resolve(toPath, name + ext)
+    
+      if (isFromPathValid && isToPathValid) await rename(fromPath, toPathWithFileName)
+      else throw new Error('There is no such directory or file')
+    }
+    catch(err) {
+      throw new Error(err)
+    }
+  }
 }
 
 export default mv
