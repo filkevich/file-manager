90e28d346300a1bfa987649e57126c5f44427b6f
Artiom Filkevich <artiom.filkevich@gmail.com>
2022-06-11 16:00:17 +0300

    fix: cat operation; rewrite it on Promises

--- files/modules/cat.js
+++ files/modules/cat.js
@@ -1,23 +1,34 @@
+import { EOL } from 'os'
+import { stdout } from 'process'
 import { createReadStream } from 'fs'
 import { pathValidator } from '../../validators/index.js'
-import { printCurrentDir } from '../../msg/index.js'
-import { EOL } from 'os'
+import { printInvalidInputMsg } from '../../msg/index.js'
 
-const cat =  args => {
-  if (args.length > 1) throw new Error('Wrong arguments quantity')
+const cat = async args => {
+  const isArgsValid = args.length === 1
+  if (!isArgsValid) printInvalidInputMsg()
+  else {
+    try {
+      const [ filePath ] = args
+      const { normalizedPath, isPathExists } = pathValidator(filePath)
 
-  const [ filePath ] = args
-  const { normalizedPath, isPathExists } = pathValidator(filePath)
-
-  if (isPathExists) {
-    const readable = createReadStream(normalizedPath)
-    readable.on('data', chunk => process.stdout.write(chunk))
-    readable.on('end', () => {
-      process.stdout.write(`${EOL}${EOL}`)
-      printCurrentDir()
-    })
+      if (isPathExists) {
+        await new Promise((resolve, reject) => {
+          const readable = createReadStream(normalizedPath)
+          readable.on('data', chunk => stdout.write(chunk))
+          readable.on('end', () => {
+            resolve()
+            stdout.write(EOL)
+          })
+          readable.on('error', err => reject(err))
+        })
+      }
+      else printInvalidInputMsg()
+    }
+    catch(err) {
+      throw new Error(err)
+    }
   }
-  else throw new Error('There is no such directory or file')
 }
 
 export default cat
