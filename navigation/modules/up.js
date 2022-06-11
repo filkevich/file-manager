10c1802a16c31d06a57bb9c0cfc79e750e977a58
Artiom Filkevich <artiom.filkevich@gmail.com>
2022-06-11 13:32:30 +0300

    refactor: up operation

--- navigation/modules/up.js
+++ navigation/modules/up.js
@@ -1,12 +1,19 @@
 import { join } from 'path'
+import { env } from 'process'
+import { printInvalidInputMsg } from '../../msg/index.js'
 
-const up = args => {
-  if (args.length === 0) {
-    process.env.CURRENT_DIR = join(process.env.CURRENT_DIR, '..')
+const up = async args => {
+  const isArgsValid = args.length === 0
+  
+  if (isArgsValid) {
+    try {
+      env.CURRENT_DIR = join(env.CURRENT_DIR, '..')
+    }
+    catch(err) {
+      throw new Error(err)
+    }
   }
-  else {
-    throw new Error('There is no arguments needed')
-  }
+  else printInvalidInputMsg()
 }
 
 export default up
