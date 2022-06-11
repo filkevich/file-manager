b33af345fa8d49d9fd3b4e81c7edc892dbd98b79
Artiom Filkevich <artiom.filkevich@gmail.com>
2022-06-11 15:43:24 +0300

    fix: remove unusefull import

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 import os from './os/index.js'
 import hash from './hash/index.js'
-import { printMessage, printCurrentDir, printInvalidInputMsg, printOperationFailedMsg } from './msg/index.js'
+import { printCurrentDir, printInvalidInputMsg, printOperationFailedMsg } from './msg/index.js'
 import { ls, up, cd } from './navigation/index.js'
 import { inputValidator } from './validators/index.js'
 import { rn, add, rm, cp, mv, cat } from './files/index.js'
