382e900934664b1f57f9e2804b4fd30f8a0940ad
Artiom Filkevich <artiom.filkevich@gmail.com>
2022-06-11 13:48:48 +0300

    refactor: cd operation

--- navigation/modules/cd.js
+++ navigation/modules/cd.js
@@ -1,13 +1,22 @@
+import { env } from 'process'
+import { printInvalidInputMsg } from '../../msg/index.js'
 import { pathValidator } from '../../validators/index.js'
 
-const cd = args => {
-  if (args.length > 1) throw new Error('Wrong arguments quantity')
-  
-  const [path] = args
-  const { normalizedPath, isPathExists } = pathValidator(path)
+const cd = async args => {
+  const isArgsValid = args.length === 1
+  if (!isArgsValid) printInvalidInputMsg()
+  else {
+    try {
+      const [path] = args
+      const { normalizedPath, isPathExists } = pathValidator(path)
 
-  if (isPathExists) process.env.CURRENT_DIR = normalizedPath
-  else throw new Error('There is no such directory')
+      if (isPathExists) env.CURRENT_DIR = normalizedPath
+      else throw new Error('There is no such directory')
+    }
+    catch(err) {
+      throw new Error(err)
+    }
+  }
 }
 
 export default cd
